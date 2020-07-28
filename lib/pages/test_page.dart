bf37370c26146ee704a5783add6e91950b64cc75
eldetulado <eldetulado@gmail.com>
2020-07-27 23:41:56 -0400

    add code in test page

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -8,12 +8,40 @@ class TestPage extends StatefulWidget {
   _TestPageState createState() => _TestPageState();
 }
 
-class _TestPageState extends State<TestPage> {
+class _TestPageState extends State<TestPage> with WidgetsBindingObserver {
   final RepoFile _repoFile = RepoFile();
   final TextEditingController controller = TextEditingController();
   final shapeButton = StadiumBorder();
 
   String dataFile;
+
+  @override
+  void initState() {
+    print("========= INIT STATE ===========");
+    _repoFile.writeFile("${DateTime.now().toString()} init State \n");
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    print("========= DISPOSE ===========");
+    _repoFile.writeFile("${DateTime.now().toString()} dispose State \n");
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      print("========= PAUSED ===========");
+      _repoFile.writeFile("${DateTime.now().toString()} paused State \n");
+    }
+    if (state == AppLifecycleState.resumed) {
+      print("========= RESUME ===========");
+      _repoFile.writeFile("${DateTime.now().toString()} resume State \n");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,13 +64,12 @@ class _TestPageState extends State<TestPage> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               RaisedButton(
-                shape: shapeButton,
-                child: Text("Escribir"),
-                onPressed: () async {
-                  await _repoFile.writeFile(controller.text.trim() ?? "nulo");
-                  this.controller.clear();
-                }
-              ).addExpanded(),
+                  shape: shapeButton,
+                  child: Text("Escribir"),
+                  onPressed: () async {
+                    await _repoFile.writeFile(controller.text.trim() ?? "nulo");
+                    this.controller.clear();
+                  }).addExpanded(),
               RaisedButton(
                 shape: shapeButton,
                 child: Text("Leer"),
